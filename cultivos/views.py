b895499be39ac7d05e21d442365c7af2b0949f13
AstSamfa <samfalla31@gmail.com>
2023-10-01 22:09:14 -0500

    Se agregan las vistas para Requisito

--- cultivos/views.py
+++ cultivos/views.py
@@ -1,3 +1,50 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
+from django import forms
+
+from models import Requisito
+
 
 # Create your views here.
+
+class RequisitoListado(ListView):
+    model = Requisito
+
+
+class RequisitoCrear(SuccessMessageMixin, CreateView):
+    model = Requisito
+    form = Requisito
+    fields = "__all__"
+    success_message = 'Requisito creado correctamente'
+
+    def get_success_url(self):
+        return reverse('leer')
+
+
+class RequisitoDetalle(DetailView):
+    model = Requisito
+
+
+class RequisitoActualizar(SuccessMessageMixin, UpdateView):
+    model = Requisito
+    form = Requisito
+    fields = "__all__"
+    success_message = 'Requisito actualizado correctamente'
+
+    def get_success_url(self):
+        return reverse('leer')
+
+
+class RequisitoEliminar(SuccessMessageMixin, DeleteView):
+    model = Requisito
+    form = Requisito
+    fields = "__all__"
+
+    def get_success_url(self):
+        success_message = 'Requisito eliminado correctamente'
+        messages.success(self.request, success_message)
+        return reverse('leer')

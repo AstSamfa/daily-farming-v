1d21b5406ad16923cf16fc771d83a6af2d32ab86
Laura <lauracast142@gmail.com>
2023-10-15 13:57:48 -0500

    eliminación carpeta home en settings

--- dailyfarming/settings.py
+++ dailyfarming/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 MEDIA_URL = '/cultivos/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'cultivos/static/home')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'cultivos/static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
